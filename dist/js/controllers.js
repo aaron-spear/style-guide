800cb7c569eca72c61cba2ee1a6a4445d0a11658
aaron-spear <aspear@spotxchange.com>
2015-01-16 13:58:25 -0700

    modals

--- dist/js/controllers.js
+++ dist/js/controllers.js
@@ -310,3 +310,61 @@
 		    isFirstDisabled: false
 		  };
 		});
+		
+		
+				
+		// Modal			
+		
+		styleApp.controller('modalCtrl', function ($scope, $modal, $log) {
+		
+		  $scope.items = ['item1', 'item2', 'item3'];
+		
+		  $scope.open = function (size) {
+		
+		    var modalInstance = $modal.open({
+		      templateUrl: 'myModalContent.html',
+		      controller: 'modalInstanceCtrl',
+		      size: size,
+		      resolve: {
+		        items: function () {
+		          return $scope.items;
+		        }
+		      }
+		    });
+		
+		    modalInstance.result.then(function (selectedItem) {
+		      $scope.selected = selectedItem;
+		    }, function () {
+		      $log.info('Modal dismissed at: ' + new Date());
+		    });
+		  };
+		});		
+
+		styleApp.controller('modalInstanceCtrl', function ($scope, $modalInstance, items) {
+		
+		  $scope.items = items;
+		  $scope.selected = {
+		    item: $scope.items[0]
+		  };
+		
+		  $scope.ok = function () {
+		    $modalInstance.close($scope.selected.item);
+		  };
+		
+		  $scope.cancel = function () {
+		    $modalInstance.dismiss('cancel');
+		  };
+		});
+
+
+
+
+
+
+
+
+
+
+
+
+
