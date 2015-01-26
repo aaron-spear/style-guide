4fae7f3d2c05e56a36c323bea635ad38afb3b6fa
aaron-spear <aspear@spotxchange.com>
2015-01-26 16:52:19 -0700

    tabs

--- dist/js/controllers.js
+++ dist/js/controllers.js
@@ -49,6 +49,17 @@
 		
 		styleApp.controller('buttonsController', function($scope) {
 	    $scope.pageClass = 'page-buttons';
+	    
+		  $scope.tabs = [
+		    { title:'Dynamic Title 1', content:'Dynamic content 1' },
+		    { title:'Dynamic Title 2', content:'Dynamic content 2' }
+		  ];
+		
+		  $scope.alertMe = function() {
+		    setTimeout(function() {
+		      $window.alert('You\'ve selected the alert tab!');
+		    });
+		  };	    
 		}); 
 		
 		styleApp.controller('tablesController', function($scope) {
